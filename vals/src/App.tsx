51fd8ef33e3e6acaa7b3805d4cc96b4b24ee4ade
The-Algorist <waruithemystery@gmail.com>
2024-02-06 21:08:19 +0300

    Add the app functions

--- vals/src/App.tsx
+++ vals/src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const phrases: string[] = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+  "Think again!",
+  "Last chance!",
+  "Surely not?",
+  "You might regret this!",
+  "Give it another thought!",
+  "Are you absolutely certain?",
+  "This could be a mistake!",
+  "Have a heart!",
+  "Don't be so cold!",
+  "Change of heart?",
+  "Wouldn't you reconsider?",
+  "Is that your final answer?",
+  "You're breaking my heart ;(",
+];
+
+function App(): JSX.Element {
+  const [noCount, setCount] = useState<number>(0);
+  const [yesPressed, setYesPressed] = useState<boolean>(false);
+  const yesButtonSize: number = noCount * 20 + 16;
+
+  function handleNoClick(): void {
+    setCount(noCount + 1);
+    // setYesPressed(false);
+  }
+
+  function getNoButtonText(): string {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {yesPressed ? (
+        <>
+          <div className="gif-container">
+            <img
+              src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+              alt="kissing bear"
+            />
+          </div>
+          <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+        </>
+      ) : (
+        <>
+          <div className="gif-container">
+            <img
+              className="h-[200px]"
+              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+              alt="bear with roses"
+            />
+          </div>
+          <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
+          <div className="btn-container">
+            <button
+              className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
